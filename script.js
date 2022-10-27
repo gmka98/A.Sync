46f3019c670b291bef6ec7ce6fc4344c6ffa7414
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-10-27 15:59:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-document.querySelector('button').addEventListener('click', function()){
-const api ="https://github.com/becodeorg/BXL-Johnson-09/blob/main/2.The-Hill/Frontend-sprint/3.Async-Fetch/becode.json"
+const body = document.getElementsByTagName('body')
+const btn = document.createElement('button')
 
-fetch(api)
-.then(reponse=> 'becode.json')
-.then(text => {
-    console.log(json)
-});
+fetch('becode.json') // No need to put fetch()with url because it's a local server
+.then(response => response.json())
+.then()
+  
+    function createListElem() {
+        const ul = document.createElement('ul')
+        document.body.appendchild('ul')
+        const liste1 = document.createElement('li');
+        const liste2 = document.createElement('li');
+        const liste3 = document.createElement('li');
+        const liste4 = document.createElement('li');
 
-}
+        ul.appendChild(liste1)
+        ul.appendChild(liste2)
+        ul.appendChild(liste3)
+        ul.appendChild(liste4)
+    
+    }
 
-<button id="btn" type="button">becode</button>
+    btn.addEventlistener('click', createListElem){
+
+    };
+
+
+   const bouton = document.createElement('button')
+   const becode = fetch('https://agify.io/')
+   
+   bouton.addEventListener('click',function ){
+    fetch(becode)
+    .then (response => becode.json() )
+    .then (becode.json)
+    }
+    catch(error => {
+        console.log('There was an error!', error)
+    })
+
+
+
+
+
+
+
+
+    
+    
+
